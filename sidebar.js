4d57b0ab86942669bc56870ae445acb17569f68b
Peeter Aleksander Randla <peeter.a.randlaa@gmail.com>
2022-12-05 21:48:19 +0200

    lisa sidebar

---
+++ sidebar.js
@@ -0,0 +1,6 @@
+let sb = document.getElementById('sidebar');
+sb.innerHTML = `<ul>
+    <li><a href="ajalugu.html">Ajalugu</a></li>
+    <li><a href="beginners_method.html">Algaja meetod</a></li>
+    <li><a href="meetodid.html">Lahendamismeetodid</a></li>
+</ul>`;
