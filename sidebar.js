b6e9be4041bc087a81f1dbb39d124a2ae5756d79
Peeter Aleksander Randla <peeter.a.randlaa@gmail.com>
2022-12-05 22:17:54 +0200

    lisa sidebar.js'le kommentaare

--- sidebar.js
+++ sidebar.js
@@ -1,15 +1,24 @@
+// leiame leheküljelt sidebar elemendi
 let sb = document.getElementById('sidebar');
+// nimekiri kõikidest lehekülgedest, mis sidebar'i peal on
+// esimene on lehekülje faili nimi, teine on pealkiri
 let pages = [
     ["ajalugu", "Ajalugu"],
     ["beginners_method", "Algaja meetod"],
     ["meetodid", "Lahendamismeetodid"]
 ];
+// sidebar html jaoks muutuja
 let out = '';
+// iga lehekülje kohta
 for(let [link,name] of pages) {
+    // vormistame lingi
     let line = `<a href="${link}.html">${name}</a></li>`;
+    // kui oleme juba praegu sellel leheküljel
     if(current_page == link) {
+        // siis paneme lingi asemel boldis pealkirja
         line = `<b>${name}</b>`;
     }
     out += `<li>${line}</li>`;
 }
+// kirjutame sidebar'i html'i üle
 sb.innerHTML = `<ul>${out}</ul>`;
